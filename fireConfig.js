113118f1850bf2e72393b83975659838b50692da
Mavrucukack <maverickagana05@gmail.com>
2025-04-05 14:15:05 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDpXJDkCxuvbZDF__R8tsGrG5GiEwzaP-8",
+  authDomain: "agana-project-e39a6.firebaseapp.com",
+  databaseURL: "https://agana-project-e39a6-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "agana-project-e39a6",
+  storageBucket: "agana-project-e39a6.firebasestorage.app",
+  messagingSenderId: "381243875129",
+  appId: "1:381243875129:web:9766409747d01ee3b212e8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
